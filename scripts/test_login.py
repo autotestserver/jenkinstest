ea78b56280e5cb3a83159b1371d397f7a005fa9f
hhh <1183065745@qq.com>
2019-01-24 16:29:23 +0800

    aaa

---
+++ scripts/test_login.py
@@ -0,0 +1,18 @@
+import allure
+
+def test_one():
+    assert 1
+
+@allure.step(title="测试登录流程")
+def test_login():
+    allure.attach('登录', '输入用户名')
+    print('abc') #输入用户名
+
+    allure.attach('登录', '输入密码')
+    print('abc')
+
+    allure.attach('登录', '点击登录')
+    print('abc')
+    assert 0
+
+
